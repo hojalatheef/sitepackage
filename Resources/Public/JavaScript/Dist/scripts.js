ef114e8c5e748db3eb9b826ff35249f7a0f63bf4
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-05-14 10:01:53 +0200

    [TASK] Added extension structure

---
+++ Resources/Public/JavaScript/Dist/scripts.js
@@ -0,0 +1,6 @@
+/*!
+ * Sitepackage v1.0.0 (https://jweiland.net/)
+ * Copyright 2017-2024 Hoja Mustaffa Abdul Latheef
+ * Licensed under the GPL-2.0-or-later license
+ */
+console.log("WE LOVE TYPO3");
---
+++ ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Extension Manager/Repository config file for ext "sitepackage".
+ */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Sitepackage',
+    'description' => 'TYPO3 Sitepackage',
+    'category' => 'templates',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.1.0-13.1.99',
+            'fluid_styled_content' => '13.1.0-13.1.99',
+            'rte_ckeditor' => '13.1.0-13.1.99',
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Jweiland\\Sitepackage\\' => 'Classes',
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Hoja Mustaffa Abdul Latheef',
+    'author_email' => 'hlatheef@jweiland.net',
+    'author_company' => 'Jweiland.net',
+    'version' => '1.0.0',
+];
